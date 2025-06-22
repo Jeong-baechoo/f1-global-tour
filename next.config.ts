c2b7ec1c635067a92d9ecac3cca6c5ff076232d8
jeong-baechoo <baechoo0905@naver.com>
2025-06-22 14:01:31 +0900

    chore: 프로젝트 설정 및 의존성 업데이트
    - IDE 파일 gitignore 추가
    - Next.js 이미지 도메인 설정
    - leaflet 및 react-leaflet 의존성 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-1.motorsport.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.formula1.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
